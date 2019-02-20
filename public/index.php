ee98a5e99d8aa3359c817b1b77a795323ea8eb01
Alex Mills <alex@asdfx.us>
2019-02-20 14:00:54 +0000

    refactored to use the Tight framework core rather than individual packages

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$router = new \Bramus\Router\Router();
+$app = new \Asdfx\Tight\Tight();
+$router = $app->getRouter();
 $router->setNamespace('App\\Http\\Controllers');
 require __DIR__ . '/../routes/web.php';
 $router->run();
