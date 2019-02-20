9256d0cfa5fe5a83fc355e54a0f81d0073d3a25d
Alex Mills <alex@asdfx.us>
2019-02-20 15:14:41 +0000

    added placeholder configurations, and view configuration

---
+++ config/logging.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 20/02/19
+ * Time: 15:12
+ */
---
+++ config/views.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'path' => __DIR__ . '/resources/views',
+    'cache' => __DIR__ . '/storage/cache/views'
+];
